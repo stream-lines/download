3b28210477a86d626865f07911be34ca2145b753
Joshua Chang <joshuatree10@gmail.com>
2020-07-05 20:33:59 -0500

    Updates

---
+++ precache-manifest.a46030e9df6d232cfffdaeeb8238ae9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bf618c8e84fb39202ac9ea9d2df2a1",
+    "url": "/download/index.html"
+  },
+  {
+    "revision": "5934b33ec76ce290d586",
+    "url": "/download/static/css/main.e529bb55.chunk.css"
+  },
+  {
+    "revision": "57af44e38adedd696201",
+    "url": "/download/static/js/2.f06290d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/download/static/js/2.f06290d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5934b33ec76ce290d586",
+    "url": "/download/static/js/main.01e7b767.chunk.js"
+  },
+  {
+    "revision": "30c627a5c2384e3e153b",
+    "url": "/download/static/js/runtime-main.a6488357.js"
+  }
+]);
